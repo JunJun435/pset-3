d74e31f6a3aa6699c04c1564a623e6cca0750c66
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-10-30 09:03:15 -0400

    Update change.js

--- src/change.js
+++ src/change.js
@@ -1,14 +1,25 @@
-const readlineSync = require("readline-sync");
+onst readlineSync = require("readline-sync");
 
 const MIN = 0.01;
 const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
-  const coinValue = [.25. .1, .05, .01] //[quarters, dimes, nickels, pennies]
+
 if (Number.isNaN(amount)) {
-    console.log("\nInvalid");
+    console.log("Invalid.");
 } else if (amount < MIN || amount > MAX) {
-    console.log("\nInvalid")
+    console.log("Invalid.");
 } else {
-    let coins;
+    const q = Math.floor(amount / 0.25);
+    let change = amount - (q * 0.25);
+
+    const d = Math.floor(change / 0.1);
+    change = change - (d * 0.1);
+
+    const n = Math.floor(change / 0.05);
+    change = change - (n * 0.05);
+
+    const p = Math.round(change / 0.01);
+    console.log(q + " quarters, " + d + " dimes, " + n + " nickels, and " + p + " pennies.");
+
 }
