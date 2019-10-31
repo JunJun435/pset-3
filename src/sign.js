2cba45cf0ff8eaacf7beab116a073e5ccf85b145
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-10-31 19:14:30 -0400

    Update sign.js

--- src/sign.js
+++ src/sign.js
@@ -4,11 +4,13 @@ const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
 const amount = Number(readlineSync.question("\nEnter a number: "));
-if (amount < MIN || amount > MAX);
-console.log("\nInvalid.")
-if (amount == 0) {
-  console.log("\nZero.");
-} else if (amount > 0) {
-  console.log("\nPositive.")
-} else
-console.log("\nNegative.");
+
+if (amount > 0) {
+  console.log("\nPositive.");
+} else if (amount < 0) {
+  console.log("\nNegative");
+} else if (amount == 0) {
+  console.log("\nZero");
+} else {
+  console.log("\nInvalid");
+}
