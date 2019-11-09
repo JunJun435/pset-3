93135b6556fce2faff5807faf24226cfc4ec8618
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-09 00:11:47 -0500

    Update years.js

--- src/years.js
+++ src/years.js
@@ -1,3 +1,18 @@
 const readlineSync = require("readline-sync");
 
-const year = readlineSync.question("\nEnter a year: ");
+const MIN = 0;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+let year = Number(readlineSync.question("\nEnter a year: "));
+
+let remainder = yearInput % 4;
+
+if (Number.isNaN(yearInput)) {
+  console.log("\nInvalid.");
+} else if (year < MIN || year > MAX) {
+  console.log("\nInvalid.");
+} else if (remainder > 0) {
+  console.log("\n" + year + " is not a leap year.");
+} else {
+  console.log("\n" + year + " is a leap year.");
+}
